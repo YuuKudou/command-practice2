c423f0c5153f889325835a9c1321155288e4cd3e
Yuu Kudou <y.kudou.sys21@morijyobi.ac.jp>
2022-03-17 12:30:37 +0900

    add Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class Main{
 	public static void main(String[]args){
 		Scanner scanner = new Scanner(System.in);
-		
+		int i;
+		i = 0;
 		System.out.println("kg");
 		int kg = scanner.nextInt();
 		
@@ -10,7 +11,11 @@ public class Main{
 		double m = scanner.nextDouble();
 		
 		double bmi = kg / (m * m);
+
+		while(i < 2){
 		System.out.println("bmi" + bmi);
+		i = i + 1;
+		}
 
 	}
 }
